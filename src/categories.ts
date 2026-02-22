bdaafd7a87410bc0a6832457e77f2cd810554345
Xander Wayne <xandernzaywa@gmail.com>
2026-02-22 11:48:52 +0300

    Complete the cv scrape

--- src/categories.ts
+++ src/categories.ts
@@ -8,110 +8,110 @@ export interface CategorySeed {
       category: 'Accounting and Finance CVs',
       categoryUrl: 'https://jiji.co.ke/accounting-and-finance-cvs',
     },
-    // {
-    //   category: 'Advertising and Marketing CVs',
-    //   categoryUrl: 'https://jiji.co.ke/advertising-and-marketing-cvs',
-    // },
-    // {
-    //   category: 'Arts and Entertainment CVs',
-    //   categoryUrl: 'https://jiji.co.ke/arts-and-entertainment-cvs',
-    // },
-    // {
-    //   category: 'Clerical and Administrative CVs',
-    //   categoryUrl: 'https://jiji.co.ke/clerical-and-administrative-cvs',
-    // },
-    // {
-    //   category: 'Construction and Skilled Trade CVs',
-    //   categoryUrl: 'https://jiji.co.ke/construction-and-skilled-trade-cvs',
-    // },
-    // {
-    //   category: 'Consulting and Strategy CVs',
-    //   categoryUrl: 'https://jiji.co.ke/consulting-and-strategy-cvs',
-    // },
-    // {
-    //   category: 'Customer Service CVs',
-    //   categoryUrl: 'https://jiji.co.ke/customer-service-cvs',
-    // },
-    // {
-    //   category: 'Driver CVs',
-    //   categoryUrl: 'https://jiji.co.ke/driver-cvs',
-    // },
-    // {
-    //   category: 'Engineering and Architecture CVs',
-    //   categoryUrl: 'https://jiji.co.ke/engineering-and-architecture-cvs',
-    // },
-    // {
-    //   category: 'Health and Beauty CVs',
-    //   categoryUrl: 'https://jiji.co.ke/health-and-beauty-cvs',
-    // },
-    // {
-    //   category: 'Gardening and Landscaping CVs',
-    //   categoryUrl: 'https://jiji.co.ke/gardening-and-landscaping-cvs',
-    // },
-    // {
-    //   category: 'Healthcare and Nursing CVs',
-    //   categoryUrl: 'https://jiji.co.ke/healthcare-and-nursing-cvs',
-    // },
-    // {
-    //   category: 'Hotel CVs',
-    //   categoryUrl: 'https://jiji.co.ke/hotel-cvs',
-    // },
-    // {
-    //   category: 'Housekeeping and Cleaning CVs',
-    //   categoryUrl: 'https://jiji.co.ke/housekeeping-and-cleaning-cvs',
-    // },
+    {
+      category: 'Advertising and Marketing CVs',
+      categoryUrl: 'https://jiji.co.ke/advertising-and-marketing-cvs',
+    },
+    {
+      category: 'Arts and Entertainment CVs',
+      categoryUrl: 'https://jiji.co.ke/arts-and-entertainment-cvs',
+    },
+    {
+      category: 'Clerical and Administrative CVs',
+      categoryUrl: 'https://jiji.co.ke/clerical-and-administrative-cvs',
+    },
+    {
+      category: 'Construction and Skilled Trade CVs',
+      categoryUrl: 'https://jiji.co.ke/construction-and-skilled-trade-cvs',
+    },
+    {
+      category: 'Consulting and Strategy CVs',
+      categoryUrl: 'https://jiji.co.ke/consulting-and-strategy-cvs',
+    },
+    {
+      category: 'Customer Service CVs',
+      categoryUrl: 'https://jiji.co.ke/customer-service-cvs',
+    },
+    {
+      category: 'Driver CVs',
+      categoryUrl: 'https://jiji.co.ke/driver-cvs',
+    },
+    {
+      category: 'Engineering and Architecture CVs',
+      categoryUrl: 'https://jiji.co.ke/engineering-and-architecture-cvs',
+    },
+    {
+      category: 'Health and Beauty CVs',
+      categoryUrl: 'https://jiji.co.ke/health-and-beauty-cvs',
+    },
+    {
+      category: 'Gardening and Landscaping CVs',
+      categoryUrl: 'https://jiji.co.ke/gardening-and-landscaping-cvs',
+    },
+    {
+      category: 'Healthcare and Nursing CVs',
+      categoryUrl: 'https://jiji.co.ke/healthcare-and-nursing-cvs',
+    },
+    {
+      category: 'Hotel CVs',
+      categoryUrl: 'https://jiji.co.ke/hotel-cvs',
+    },
+    {
+      category: 'Housekeeping and Cleaning CVs',
+      categoryUrl: 'https://jiji.co.ke/housekeeping-and-cleaning-cvs',
+    },
   
-    // {
-    //   category: 'Human Resources CVs',
-    //   categoryUrl: 'https://jiji.co.ke/human-resources-cvs',
-    // },
+    {
+      category: 'Human Resources CVs',
+      categoryUrl: 'https://jiji.co.ke/human-resources-cvs',
+    },
   
-    // {
-    //   category: 'Internship CVs',
-    //   categoryUrl: 'https://jiji.co.ke/internship-cvs',
-    // },
+    {
+      category: 'Internship CVs',
+      categoryUrl: 'https://jiji.co.ke/internship-cvs',
+    },
   
-    // {
-    //   category: 'Legal CVs',
-    //   categoryUrl: 'https://jiji.co.ke/legal-cvs',
-    // },
+    {
+      category: 'Legal CVs',
+      categoryUrl: 'https://jiji.co.ke/legal-cvs',
+    },
   
-    // {
-    //   category: 'Logistics and Transportation CVs',
-    //   categoryUrl: 'https://jiji.co.ke/logistics-and-transportation-cvs',
-    // },
-    // {
-    //   category: 'Sales and Telemarketing CVs',
-    //   categoryUrl: 'https://jiji.co.ke/sales-and-telemarketing-cvs',
-    // },
-    // {
-    //   category: 'Travel and Tourism CVs',
-    //   categoryUrl: 'https://jiji.co.ke/travel-and-tourism-cvs',
-    // },
+    {
+      category: 'Logistics and Transportation CVs',
+      categoryUrl: 'https://jiji.co.ke/logistics-and-transportation-cvs',
+    },
+    {
+      category: 'Sales and Telemarketing CVs',
+      categoryUrl: 'https://jiji.co.ke/sales-and-telemarketing-cvs',
+    },
+    {
+      category: 'Travel and Tourism CVs',
+      categoryUrl: 'https://jiji.co.ke/travel-and-tourism-cvs',
+    },
   
-    // {
-    //   category: 'Technology CVs',
-    //   categoryUrl: 'https://jiji.co.ke/technology-cvs',
-    // },
+    {
+      category: 'Technology CVs',
+      categoryUrl: 'https://jiji.co.ke/technology-cvs',
+    },
   
-    // {
-    //   category: 'Management CVs',
-    //   categoryUrl: 'https://jiji.co.ke/management-cvs',
-    // },
+    {
+      category: 'Management CVs',
+      categoryUrl: 'https://jiji.co.ke/management-cvs',
+    },
   
-    // {
-    //   category: 'Office CVs',
-    //   categoryUrl: 'https://jiji.co.ke/office-cvs',
-    // },
-    // {
-    //   category: 'Retail CVs',
-    //   categoryUrl: 'https://jiji.co.ke/retail-cvs',
-    // },
+    {
+      category: 'Office CVs',
+      categoryUrl: 'https://jiji.co.ke/office-cvs',
+    },
+    {
+      category: 'Retail CVs',
+      categoryUrl: 'https://jiji.co.ke/retail-cvs',
+    },
   
-    // {
-    //   category: 'Other CVs',
-    //   categoryUrl: 'https://jiji.co.ke/other-cvs',
-    // },
+    {
+      category: 'Other CVs',
+      categoryUrl: 'https://jiji.co.ke/other-cvs',
+    },
   
     // Add more:
     // { category: 'Web Designers', categoryUrl: 'https://jiji.co.ke/...' },
